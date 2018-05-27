67d3746a20c8240a0a06f10fed7ce211286d45cc
Ibrahim Attalla <ibrahimattalla@Ibrahims-Mac.local>
2018-05-27 13:55:33 -0700

    Initial Commit

---
+++ Pokedix3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedix3
+//
+//  Created by Ibrahim Attalla on 5/27/18.
+//  Copyright © 2018 Ibrahim Attalla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
